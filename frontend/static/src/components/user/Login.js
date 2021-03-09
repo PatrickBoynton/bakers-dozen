09aad1f9e7b4b37352cbf3f90f290997856f289c
PatrickBoynton <jpboynton3@gmail.com>
2021-03-09 11:22:00 -0500

    Moved the login form to it's own component.

---
+++ frontend/static/src/components/user/Login.js
@@ -0,0 +1,22 @@
+import {Component} from 'react';
+
+class Login extends Component {
+    render() {
+        return <form action="">
+            <label htmlFor="servings">Servings</label>
+            <input type="number" name="servings"/>
+            <select>
+                <option value="Fahrenheit">F</option>
+                <option value="Celsius">C</option>
+            </select>
+            <label htmlFor="ingredients">Ingredients</label>
+            <input type="text" name="ingredients"/>
+            <button>Add Ingredient</button>
+            <label htmlFor="measurements">Measurements</label>
+            <input type="text" name="measurements"/>
+            <button type="submit">Add Recipe</button>
+        </form>;
+    }
+}
+
+export default Login;
