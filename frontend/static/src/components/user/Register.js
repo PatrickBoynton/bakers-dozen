c288481092a7a56d9d6d393d924987aff89dd4c8
PatrickBoynton <jpboynton3@gmail.com>
2021-03-09 11:24:10 -0500

    Moved the register to the component.

---
+++ frontend/static/src/components/user/Register.js
@@ -0,0 +1,21 @@
+import {Component} from 'react';
+
+class Register extends Component {
+    render() {
+        return (
+            <form action="">
+                <label htmlFor="username">Username</label>
+                <input type="text"/>
+                <label htmlFor="email">Email</label>
+                <input type="text"/>
+                <label htmlFor="password1">Password</label>
+                <input type="text"/>
+                <label htmlFor="password2">Confirm Password</label>
+                <input type="text"/>
+                <button type="submit">Register</button>
+            </form>
+        );
+    }
+}
+
+export default Register
