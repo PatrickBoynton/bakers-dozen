b879303efa3dcdc31847979dcec9ccec0e1cd3fc
PatrickBoynton <jpboynton3@gmail.com>
2021-03-09 11:29:08 -0500

    Moved all the forms to their own component.

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -12,30 +12,12 @@ class App extends Component {
             measurements: ['teaspoon', 'cup', 'pounds', 'pint'],
         };
     }
+
     render() {
         return (
             <div className="App">
-                {/*Ingredients form*/}
-                <form action="">
-                    <label htmlFor="servings">Servings</label>
-                    <input type="number" name="servings"/>
-                    <select>
-                        <option value="Fahrenheit">F</option>
-                        <option value="Celsius">C</option>
-                    </select>
-                    <label htmlFor="ingredients">Ingredients</label>
-                    <input type="text" name="ingredients"/>
-                    <button>Add Ingredient</button>
-                    <label htmlFor="measurements">Measurements</label>
-                    <input type="text" name="measurements"/>
-                    <button type="submit">Add Recipe</button>
-                </form>
-            {/* Notes form. */}
-                <form action="">
-                    <label htmlFor="notes">Notes</label>
-                    <textarea name="notes"></textarea>
-                    <button type="submit">Add note</button>
-                </form>
+                {/* Notes form. */}
+
             </div>
         );
     }
