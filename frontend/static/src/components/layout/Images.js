109d567528c721ff4810fa354de736c4155e6a08
PatrickBoynton <jpboynton3@gmail.com>
2021-03-10 17:00:44 -0500

    Working on formatting the header.

--- frontend/static/src/components/layout/Images.js
+++ frontend/static/src/components/layout/Images.js
@@ -3,13 +3,19 @@ import pizza from '../../images/lavi-perchik-LAJFSTJ5H-w-unsplash.jpg';
 import fish from '../../images/mariana-medvedeva-iNwCO9ycBlc-unsplash.jpg';
 import burger from '../../images/taylor-harding-8UjWV6iptCk-unsplash.jpg';
 import salad from '../../images/brooke-lark-jUPOXXRNdcA-unsplash (1).jpg';
-class Images extends Component{
+
+class Images extends Component {
     constructor(props) {
         super(props);
     }
+
     render() {
         return <>
-            <p>{this.props.name}</p> <hr/> <p>view all ›</p>
+            <p>{this.props.name}</p>
+            <div className="d-flex">
+                <hr/>
+                <p>view all ›</p>
+            </div>
             <div className="d-flex justify-content-center">
                 <input type="file"/>
                 <img className="img-thumbnail img-fluid thumb" src={pizza} alt="food"/>
@@ -17,8 +23,8 @@ class Images extends Component{
                 <img className="img-thumbnail img-fluid thumb" src={burger} alt="food2"/>
                 <img className="img-thumbnail img-fluid thumb" src={salad} alt="food3"/>
             </div>
-        </>
+        </>;
     }
 }
 
-export default Images
+export default Images;
