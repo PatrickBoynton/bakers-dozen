71e3a1bf2bf79100a9297d79b198d6a4fca8ede0
PatrickBoynton <jpboynton3@gmail.com>
2021-03-11 15:20:42 -0500

    Added the views.

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from . import models
+from rest_framework import generics, permissions, serializers
+
+from .serializers import RecipeSerializer
+
 
 # Create your views here.
+class RecipListView(generics.ListCreateAPIView):
+    queryset = models.Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class RecipeDetailView(generics.RetrieveAPIView):
+    lookup_field = 'id'
+    queryset = models.Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+
+class RecipeUpdateView(generics.UpdateAPIView):
+    lookup_field = 'id'
+    queryset = models.Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+
+class RecipeDeleteView(generics.DestroyAPIView):
+    lookup_field = 'id'
+    queryset = models.Recipe.objects.all()
+    serializer_class = RecipeSerializer
