70a388e506ae56e589c94656cffa5d1044d9de91
PatrickBoynton <jpboynton3@gmail.com>
2021-03-11 15:20:06 -0500

    Added the recipe model.

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db import models
+
 
 # Create your models here.
+class Recipe(models.Model):
+    recipe_name = models.CharField(max_length=50)
+    full_name = models.CharField(max_length=100)
+    visibility = models.BooleanField()
+    recipe_type = models.CharField()
+    prep_time = models.IntegerField()
+    cook_time = models.IntegerField()
+    cook_temp = models.IntegerField()
+    cook_unit = models.CharField(max_length=1)
+    yields = models.IntegerField()
+    ingredients = models.JSONField()
+    notes = models.CharField()
