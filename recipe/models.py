461bcc571081729b204199938c63748d82b3ed81
PatrickBoynton <jpboynton3@gmail.com>
2021-03-12 00:20:31 -0500

    Added recipe type.

--- recipe/models.py
+++ recipe/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.db import models
 
 
 # Create your models here.
 class Recipe(models.Model):
     title = models.CharField(max_length=50)
     name = models.CharField(max_length=100)
+    recipe_type = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
-        return self.recipe_name
+        return self.title
