815758abd4440f76eb9f396e7ce9ad282d664141
PatrickBoynton <jpboynton3@gmail.com>
2021-03-11 22:28:58 -0500

    Added a dummy fetch request for now.

--- frontend/static/src/components/layout/recipe/RecipeDisplay.js
+++ frontend/static/src/components/layout/recipe/RecipeDisplay.js
@@ -1,6 +1,16 @@
 import {Component} from 'react';
 
 class RecipeDisplay extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    async componentDidMount() {
+        const response = await fetch(`/api/v1/recipes/1/`);
+        const data = await response.json();
+        console.log(data);
+    }
+
     render() {
         return (
             <div>
