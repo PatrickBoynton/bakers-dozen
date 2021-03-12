0755e7223bac96c3558d7ea344f5403c4e784161
PatrickBoynton <jpboynton3@gmail.com>
2021-03-12 13:19:04 -0500

    Added the navigation.

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -4,11 +4,13 @@ import {Switch, Route} from 'react-router-dom';
 import RecipeList from './components/layout/recipe/RecipeList';
 import RecipeDisplay from './components/layout/recipe/RecipeDisplay';
 import Ingredients from './components/layout/recipe/Ingredients';
+import Navigation from './components/layout/Navigation/Navigation';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <Navigation/>
                 <Switch>
                     <Route path="/recipes" component={RecipeList}/>
                     <Route path="/recipe/:id">
