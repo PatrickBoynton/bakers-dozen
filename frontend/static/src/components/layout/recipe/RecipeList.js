e6e07c1fb835a7c9c39291663a4e27790243aae5
PatrickBoynton <jpboynton3@gmail.com>
2021-03-11 22:29:10 -0500

    Added a recipe list.

---
+++ frontend/static/src/components/layout/recipe/RecipeList.js
@@ -0,0 +1,29 @@
+import {Component} from 'react';
+
+class RecipeList extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            recipes: [],
+        }
+    }
+    async componentDidMount() {
+        const recipeList = await fetch("/api/v1/recipes");
+        const data = await recipeList.json();
+        this.setState({recipes: data})
+    }
+
+    render() {
+        const recipes = this.state.recipes?.map(recipe => <section>
+            <h2>{recipe.title}</h2>
+            <p>{recipe.name}</p>
+        </section> );
+        return (
+            <div>
+                {recipes}
+            </div>
+        );
+    }
+}
+
+export default RecipeList
