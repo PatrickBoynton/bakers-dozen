8a9c45c9c4d9d11c277132cec3779999b98ed636
PatrickBoynton <jpboynton3@gmail.com>
2021-03-11 22:03:44 -0500

    Added a recipe display.

---
+++ frontend/static/src/components/layout/recipe/RecipeDisplay.js
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+
+class RecipeDisplay extends Component {
+    render() {
+        return (
+            <div>
+                RecipeDisplay
+            </div>
+        );
+    }
+}
+
+export default RecipeDisplay;
