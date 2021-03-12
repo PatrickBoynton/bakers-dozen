5f057fce656e3bce7d21d72911881fc83bdc6e16
PatrickBoynton <jpboynton3@gmail.com>
2021-03-11 21:18:20 -0500

    Removed unused components.

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -1,8 +1,5 @@
 import './App.css';
 import {Component} from 'react';
-import Navigation from './components/layout/Navigation/Navigation';
-import TopNavigation from './components/layout/Navigation/TopNavigation';
-import BatchAdjust from './components/layout/recipe/BatchAdjust';
 import Ingredients from './components/layout/recipe/Ingredients';
 
 class App extends Component {
