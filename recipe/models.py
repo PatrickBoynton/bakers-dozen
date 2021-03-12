7525b2f7535b843d1e1e9c3f47d009a3fd89cd1d
PatrickBoynton <jpboynton3@gmail.com>
2021-03-11 20:42:20 -0500

    Going to redo the model.

--- recipe/models.py
+++ recipe/models.py
@@ -4,14 +4,6 @@ from django.db import models
 
 # Create your models here.
 class Recipe(models.Model):
-    recipe_name = models.CharField(max_length=50)
-    full_name = models.CharField(max_length=100)
-    visibility = models.BooleanField()
-    recipe_type = models.CharField()
-    prep_time = models.IntegerField()
-    cook_time = models.IntegerField()
-    cook_temp = models.IntegerField()
-    cook_unit = models.CharField(max_length=1)
-    yields = models.IntegerField()
-    ingredients = models.JSONField()
-    notes = models.CharField()
+
+    def __str__(self):
+        return self.recipe_name
