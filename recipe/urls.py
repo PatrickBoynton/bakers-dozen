6b421c2b916936d6d555976e5a5b3b1a27fa8cbf
PatrickBoynton <jpboynton3@gmail.com>
2021-03-11 20:45:46 -0500

    Removed the unused paths.

--- recipe/urls.py
+++ recipe/urls.py
@@ -6,6 +6,5 @@ app_name = 'recipes'
 urlpatterns = [
     path('', views.RecipListView.as_view()),
     path('<int:pk>/', views.RecipeDetailView.as_view()),
-    path('edit/<int:pk>/', views.RecipeUpdateView.as_view()),
-    path('delete/<int:pk>/', views.RecipeDeleteView.asView())
+    path('edit/<int:pk>/', views.RecipeUpdateView.as_view())
 ]
