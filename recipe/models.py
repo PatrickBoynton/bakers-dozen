fd94c3205d1f2e947f2a7c5f56c7a82dda60b35a
PatrickBoynton <jpboynton3@gmail.com>
2021-03-12 14:52:04 -0500

    Updated the model.

--- recipe/models.py
+++ recipe/models.py
@@ -6,6 +6,7 @@ class Recipe(models.Model):
     title = models.CharField(max_length=50)
     name = models.CharField(max_length=100)
     recipe_type = models.CharField(max_length=20, null=True, blank=True)
+    is_public = models.BooleanField()
 
     def __str__(self):
         return self.title
