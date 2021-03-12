58398beb2afdb909811c17a418866020fed10078
PatrickBoynton <jpboynton3@gmail.com>
2021-03-11 21:16:59 -0500

    Removed the state.

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -6,17 +6,6 @@ import BatchAdjust from './components/layout/recipe/BatchAdjust';
 import Ingredients from './components/layout/recipe/Ingredients';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            servings: 2,
-            unitTypes: ['Metric', 'US'],
-            units: ['C', 'F'],
-            items: ['carrots', 'apples', 'bread', 'celery', 'rice'],
-            measurements: ['teaspoon', 'cup', 'pounds', 'pint'],
-        };
-    }
-
     render() {
         return (
             <div className="App">
