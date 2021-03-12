07af39b59e15eaa6346f467be88056ab7eb885e6
PatrickBoynton <jpboynton3@gmail.com>
2021-03-11 21:06:49 -0500

    Started with a simple model.

--- recipe/models.py
+++ recipe/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 # Create your models here.
 class Recipe(models.Model):
+    title = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.recipe_name
