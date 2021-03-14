598b0b215182e52164f503ce1ad0136aef5edde0
PatrickBoynton <jpboynton3@gmail.com>
2021-03-13 23:01:59 -0500

    Added an about me field.

--- accounts/models.py
+++ accounts/models.py
@@ -12,6 +12,7 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE)
     profile_picture = models.ImageField(upload_to='profiles', null=True)
+    about_me = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.user.userame
