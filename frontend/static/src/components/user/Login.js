4cf6a037242c9b5da03a2d5940fed60fcdb15087
PatrickBoynton <jpboynton3@gmail.com>
2021-03-13 22:16:40 -0500

    Added the labels.

--- frontend/static/src/components/user/Login.js
+++ frontend/static/src/components/user/Login.js
@@ -4,8 +4,11 @@ class Login extends Component {
     render() {
         return (
             <form>
+                <label htmlFor="username">Username</label>
                 <input type="text" name="username" id=""/>
+                <label htmlFor="email">Email</label>
                 <input type="email" name="email" id=""/>
+                <label htmlFor="password">Password</label>
                 <input type="password" name="password" id=""/>
                 <button className="btn btn-primary" type="submit">Login</button>
             </form>
